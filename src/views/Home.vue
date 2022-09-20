20e4bbe6be920e03159a4061ae504937493f66b0
adelar888 <adelar5781@gmail.com>
2022-09-20 16:21:23 -0300

    commit lembretes/info/home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,53 +1,118 @@
 <template>
-    <v-app>
-            <div class="pa-5">
-            <div><span class="text-h3"><strong>Bem vindo</strong></span></div>
-            <div class="my-2 text-h6" v-show="date" style="color:gray;padding-left:3px;"><v-icon class="mr-2">mdi-calendar-blank</v-icon>{{ date }}</div>
-            <v-divider></v-divider>
-            <v-btn to="/Venda" class="ma-2 mt-5" fab dark color="success"><v-icon dark>mdi-basket-unfill</v-icon></v-btn>
-            <v-btn to="/Produtos" class="ma-2 mt-5" fab dark color="primary"><v-icon dark>mdi-basket-plus</v-icon></v-btn>
-            <v-btn to="/Lembretes" class="ma-2 mt-5" fab dark color="purple"><v-icon dark>mdi-clipboard-outline</v-icon></v-btn>
-            <v-btn to="/Info" class="ma-2 mt-5" fab dark color="info"><v-icon dark>mdi-store-cog</v-icon></v-btn>
-            <br>
-            <br>
-            <div>
-                <v-alert
-                color="primary"
-                dense
-                border="left"
-                elevation="5"
-                colored-border
-                class="font-weight-medium"
-                >
-                  Este site foi desenvolvido por estudantes de TI do IFC com o objetivo de melhorar o gerenciamento de vendas para lojas de açaí, um sistema simples, prático e organizado para acelerar os processos de suas vendas, deixando você no total controle, contando com históricos, informações de venda e listagem de produtos.
-                </v-alert>
-            </div>
-            <br>
-            <v-alert color="success" colored-border elevation="5" border="left"><span class="text-h5"><strong>❝ Faça o que você pode, com o que você tem, onde você estiver. ❞</strong></span>
-            <br>
-            <span style="color:gray;margin-left:10px;">- Theodore Roosevelt</span>
-            </v-alert>
-            </div>
-    </v-app>
-</template>
-<script>
+  <v-app>
+    <div class="root"><img class="background" ref="background" src="../assets/accai.png" /></div>
+    <div class="pa-5">
+      <v-divider></v-divider>
+      <div class="ml-2 mr-2">
+        <v-card outlined color="transparent" class="ml-6 mr-6 my-2" style="color:black;padding-left:3px;">
+          <span elevation="5" class="text-h3"><strong elevation="5">Bem vindo</strong></span>
+          <br>
+          <v-icon elevation="5" color="gray" class="mr-2">mdi-calendar-blank</v-icon>{{ date }}
+        </v-card>
+      </div>
+      <br>
+      <br>
+      <div class="ml-6 mr-6">
+        <v-alert outlined color="transparent" dense border="left" elevation="5" colored-border
+          class="font-weight-medium">
+          Este site foi desenvolvido por estudantes de TI do IFC com o objetivo de melhorar o gerenciamento de vendas
+          para lojas de açaí, um sistema simples, prático e organizado para acelerar os processos de suas vendas,
+          deixando você no total controle, contando com históricos, informações de venda e listagem de produtos.
+        </v-alert>
+      </div>
+      <br>
+      <div class="ml-6 mr-6">
+        <v-alert outlined color="transparent" colored-border elevation="5" border="left">
+          <span class="text-h5"><strong>❝Faça o que você pode, com o que você tem, onde você estiver. ❞</strong></span>
+          <br>
+          <span style="color:gray;margin-left:10px;">- Theodore Roosevelt</span>
+        </v-alert>
+      </div>
+      <br>
+      <br>
+      <!--TESTE                   TESTE                           TESTE-->
 
+      
+
+  </div>
+<img class="foreground" ref="foreground" src="../assets/accai.png" />
+<v-footer dark padless>
+        <v-card flat tile class="black white--text py-5 px-5 text-center" width="100%">
+            <v-row>
+                <v-col cols="12" sm="4">
+                    <v-card-text class="white--text pt-2">
+                        <v-btn icon>
+                            <v-icon color="#A7121D">fas fa-dragon</v-icon>
+                        </v-btn>
+                    </v-card-text>
+                </v-col>
+
+                <v-col cols="12" sm="4">
+                    <v-card-text class="pt-2">
+                        <v-btn v-for="icon in icons" :key="icon" class="mx-1 white--text" icon>
+                            <v-icon size="24px">
+                                {{ icon }}
+                            </v-icon>
+                        </v-btn>
+                    </v-card-text>
+                </v-col>
+                <v-col cols="12" sm="4">
+                    <v-card-text class="pt-2">
+                        <v-btn class="mx-1 white--text" icon>
+                            <v-icon color="#A7121D">mdi-heart</v-icon>
+                        </v-btn>
+                        Created by AAE IdeaPro
+                    </v-card-text>
+                </v-col>
+            </v-row>
+        </v-card>
+    </v-footer>
+</v-app>
+</template>
+
+<script>
 export default {
-    data(){
-        return{
-            date: '',
-            timerCount: '',
-        }
+  data() {
+    return {
+      date: '',
+      timerCount: '',
+      image: "@/assets/dog.png",
+      links: [
+        'Home',
+        'About Us',
+        'Team',
+        'Services',
+        'Blog',
+        'Contact Us',
+      ],
+    }
+  },
+  methods: {
+    printDate: function () {
+      return new Date().toLocaleDateString();
     },
-        methods: {
-          printDate: function () {
-            return new Date().toLocaleDateString();
-          },
-        },
-        mounted: function () {
-          this.date = this.printDate();
-        },
+  },
+  mounted: function () {
+    this.date = this.printDate();
+  },
 }
 </script>
 <style>
+img.background,
+img.foreground {
+  /* Fill background */
+  min-height: 100%;
+  min-width: 1024px;
+
+  /* Scale proportionately */
+  width: 100%;
+  height: auto;
+}
+
+img.background {
+  /* Positioning */
+  position: fixed;
+  top: 0;
+  left: 0;
+}
 </style>
--- src/views/Lembretes.vue
+++ src/views/Lembretes.vue
@@ -1,22 +1,97 @@
 <template>
   <v-app>
     <div class="pa-5">
-      <div style="border-left-style:solid;border-left-color:purple;border-left-width:8px;padding-left:10px;"
+      <div style="border-left-style:solid;border-left-color:#81D4FA;border-left-width:8px;padding-left:10px;"
         class="text-h4 my-8">Crie seus lembretes aqui!</div>
       <div style="color:gray;" class="h5 mt-2 mb-4">
-        <v-alert outlined type="info" color="purple">Clique em <v-icon color="purple">mdi-card-plus-outline</v-icon>
+        <v-alert outlined type="info" color="#81D4FA">Clique em <v-icon color="#81D4FA">mdi-card-plus-outline</v-icon>
           para criar seus lembretes.</v-alert>
       </div>
-      <div class="text-h2 mt-10 purple--text">Lembretes</div>
+      <div class="text-h2 mt-10">Lembretes</div>
       <v-card outlined class="mt-8">
-        <!--JANELA DO LEMBRETE-->
-        <v-card-title>
+        <v-card-title></v-card-title>
+
+        <!--Lembretes //////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\-->
+        <v-col>
+          <v-row>
+        <v-card-text>
+          <v-card class="font-size: 16px font-weight-black text-uppercase black--text rounded-xl" color="#81D4FA" dark
+            max-width="400" elevation="13" rounded-lg rounded-t-0>
+            <v-card-title class="light-blue lighten-2 pa-3"><span class="text-h6 font-weight-light">
+                <h3>Pagar o fornecedor</h3>
+              </span></v-card-title>
+            <v-card-text color="#212121" class="white--text text-h5 font-weight-bold mt-2">
+              <h5>"Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type
+                invalid as well."</h5>
+            </v-card-text>
+            <v-card-actions>
+              <v-list-item class="grow">
+                <v-list-item-content>
+                  <v-list-item-title>
+                    <v-chip class="ma-2" color="black--text white" label>
+                      <v-icon left color="black">mdi-account-circle-outline</v-icon>John Leider
+                    </v-chip>
+                  </v-list-item-title>
+                </v-list-item-content>
+                <v-btn ripple="false" @click="dialogConcluir = !dialogConcluir" icon color="white" class="pa-5">
+                  <v-icon fab dark class="mr-1 pa-3" large>mdi-check-bold</v-icon>
+                </v-btn>
+              </v-list-item>
+            </v-card-actions>
+          </v-card>
+        </v-card-text>
+        </v-row>
+        </v-col>
+
+        <v-col>
+          <v-row>
+        <v-card-text>
+          <v-card class="font-size: 16px font-weight-black text-uppercase black--text rounded-xl" color="#81D4FA" dark
+            max-width="400" elevation="13" rounded-lg rounded-t-0>
+            <v-card-title class="light-blue lighten-2 pa-3"><span class="text-h6 font-weight-light">
+                <h3>Pagar o fornecedor</h3>
+              </span></v-card-title>
+            <v-card-text color="#212121" class="white--text text-h5 font-weight-bold mt-2">
+              <h5>"Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type
+                invalid as well."</h5>
+            </v-card-text>
+            <v-card-actions>
+              <v-list-item class="grow">
+                <v-list-item-content>
+                  <v-list-item-title>
+                    <v-chip class="ma-2" color="black--text white" label>
+                      <v-icon left color="black">mdi-account-circle-outline</v-icon>John Leider
+                    </v-chip>
+                  </v-list-item-title>
+                </v-list-item-content>
+                <v-btn ripple="false" @click="dialogConcluir = !dialogConcluir" icon color="white" class="pa-5">
+                  <v-icon fab dark class="mr-1 pa-3" large>mdi-check-bold</v-icon>
+                </v-btn>
+              </v-list-item>
+            </v-card-actions>
+          </v-card>
+        </v-card-text>
+        </v-row>
+        </v-col>
+
+      </v-card>
+    </div>
+    <v-dialog v-model="dialogConcluir" persistent max-width="300px">
+      <v-card color="blue-grey darken-2">
+        <v-card-title class="white--text">Concluir Lembrete?</v-card-title>
+        <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn fab dark class="light-blue lighten-2" @click="dialog = !dialog">
-            <v-icon dark>mdi-card-plus-outline</v-icon>
-          </v-btn>
-        </v-card-title>
-        <v-dialog v-model="dialog" max-width="500px">
+          <v-btn text class="error white--text" @click="dialogConcluir = false">Não</v-btn>
+          <v-btn class="success" @click="doneLembrete()">Sim</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-btn fixed fab large dark bottom right class="ma-5 light-blue lighten-2" @click="dialog = !dialog">
+      <v-icon>mdi-card-plus-outline</v-icon>
+    </v-btn>
+    <!--JANELA DO LEMBRETE-->
+    <v-dialog v-model="dialog" max-width="500px">
           <v-card>
             <v-card-title class="light-blue lighten-2 pa-5">
               <v-icon color="white">mdi-text-box</v-icon>
@@ -33,8 +108,12 @@
               </v-textarea>
               <v-container fluid full-width single-line>
                 <v-row align="center">
-                  <v-col class="d-flex" cols="12" sm="12">
-                    <v-select :items="items " v-model="nomePara" label="Para" outlined></v-select>
+                  <v-col class="d-flex" cols="12" sm="6">
+                    <v-select :items="items" v-model="nomeDe" label="De" outlined>
+                    </v-select>
+                  </v-col>
+                  <v-col class="d-flex" cols="12" sm="6">
+                    <v-select :items="items" v-model="nomePara" label="Para" outlined></v-select>
                   </v-col>
                 </v-row>
               </v-container>
@@ -45,43 +124,10 @@
             </v-form>
           </v-card>
         </v-dialog>
-        <v-card-text>
-          <v-card class="" color="#81D4FA" dark max-width="400" elevation="13" rounded-lg rounded-t-0>
-            <v-card-title class="light-blue lighten-2 pa-3"><span class="text-h6 font-weight-light">Pagar o fornecedor</span></v-card-title>
-            <v-card-text color="#212121" class="text-h5 font-weight-bold mt-2">
-              "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
-            </v-card-text>
-            <v-card-actions>
-              <v-list-item class="grow">
-                <v-list-item-content>
-                  <v-list-item-title>
-                    <v-chip class="ma-2" color="#4FC3F7" label>
-                      <v-icon left>mdi-account-circle-outline</v-icon>John Leider
-                    </v-chip>
-                  </v-list-item-title>
-                </v-list-item-content>
-                <v-btn ripple="false" @click="dialogConcluir = !dialogConcluir" icon color="white" class="pa-5">
-                  <v-icon fab dark class="mr-1 pa-3" large>mdi-check-bold</v-icon>
-                </v-btn>
-              </v-list-item>
-            </v-card-actions>
-          </v-card>
-        </v-card-text>
-
-      </v-card>
-    </div>
-    <v-dialog v-model="dialogConcluir" persistent max-width="300px">
-      <v-card color="blue-grey darken-2">
-        <v-card-title class="white--text">Concluir Lembrete?</v-card-title>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn text class="error white--text" @click="dialogConcluir = false">Não</v-btn>
-          <v-btn class="success" @click="doneLembrete()">Sim</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+    
   </v-app>
 </template>
+
 <script>
 //import * as fb from '@/plugins/firebase'
 export default {
@@ -92,13 +138,17 @@ export default {
       dialogConcluir: false,
       items: ['Gerente', 'Dono', 'Vendedor(a)'],
       lembrete: [],
+
     }
   },
+
   mounted() {
     this.buscarLembretes();
   },
+
   methods: {
     salvarLembrete() {
+
     },
     concLembrete() {
       this.dialogConcluir = true
